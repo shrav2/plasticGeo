b414d18fbc82b76c68e46f9ecb5d8d6bb6ea10bf
Shravani Subramanian <ssubra20@calpoly.edu>
2026-05-24 16:44:01 -0700

    edits in description and load_data

---
+++ R/load_data.R
@@ -0,0 +1,5 @@
+load_data <- function(){
+  readr::read_csv('https://raw.githubusercontent.com/shrav2/Environmental-Plastic-Waste-Analysis/refs/heads/main/stat431-meta.csv')
+  readr::read_csv('https://raw.githubusercontent.com/shrav2/Environmental-Plastic-Waste-Analysis/refs/heads/main/OutsidePlasticData.csv')
+
+}
