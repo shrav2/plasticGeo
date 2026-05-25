d11c45eecf8536c63984220512b8b2ffd917c447
Shravani Subramanian <ssubra20@calpoly.edu>
2026-05-24 17:19:16 -0700

    updates load_data and description

--- R/load_data.R
+++ R/load_data.R
@@ -1,17 +1,27 @@
-#' Load Plastic Waste Dataset & Nigeria Dataset
+#' Load Plastic Waste Dataset
 #'
 #' Downloads the environmental plastic waste dataset
 #' used for temperature and cleanup analysis from the Break Free From Plastic movement.
-#' Also downloads outside data from the Earth Challenge 2020 for beach-cleanups in Nigeria.
 #'
-#' @return A tibble containing plastic waste and
-#' temperature data & Nigeria beach-cleanup data
+#' @return A tibble containing plastic waste collection data from the Break Free From Plastic movement.
 #'
 #' @importFrom readr read_csv
 #'
 #' @export
-load_data <- function(){
-  readr::read_csv('https://raw.githubusercontent.com/shrav2/Environmental-Plastic-Waste-Analysis/refs/heads/main/stat431-meta.csv')
-  readr::read_csv('https://raw.githubusercontent.com/shrav2/Environmental-Plastic-Waste-Analysis/refs/heads/main/Nigeria%20Data.csv')
+load_plastic_data <- function(){
+   readr::read_csv("https://raw.githubusercontent.com/shrav2/Environmental-Plastic-Waste-Analysis/main/stat431-meta.csv")
+}
+
+#' Load Nigeria Dataset
+#'
+#' Downloads outside data from the Earth Challenge 2020 for beach-cleanups in Nigeria.
+#'
+#' @return A tibble containing plastic waste collection data from the Break Free From Plastic movement.
+#'
+#' @importFrom readr read_csv
+#'
+#' @export
+load_nigeria_data <- function(){
+   readr::read_csv("https://raw.githubusercontent.com/shrav2/Environmental-Plastic-Waste-Analysis/main/Nigeria%20Data.csv")
 
 }
