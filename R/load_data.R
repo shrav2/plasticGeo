8e5aa66b9ee18ab067939f09f901176aa2b54b3c
Shravani Subramanian <ssubra20@calpoly.edu>
2026-05-24 17:08:58 -0700

    added Nigeria dataset

--- R/load_data.R
+++ R/load_data.R
@@ -1,5 +1,17 @@
+#' Load Plastic Waste Dataset & Nigeria Dataset
+#'
+#' Downloads the environmental plastic waste dataset
+#' used for temperature and cleanup analysis from the Break Free From Plastic movement.
+#' Also downloads outside data from the Earth Challenge 2020 for beach-cleanups in Nigeria.
+#'
+#' @return A tibble containing plastic waste and
+#' temperature data & Nigeria beach-cleanup data
+#'
+#' @importFrom readr read_csv
+#'
+#' @export
 load_data <- function(){
   readr::read_csv('https://raw.githubusercontent.com/shrav2/Environmental-Plastic-Waste-Analysis/refs/heads/main/stat431-meta.csv')
-  readr::read_csv('https://raw.githubusercontent.com/shrav2/Environmental-Plastic-Waste-Analysis/refs/heads/main/OutsidePlasticData.csv')
+  readr::read_csv('https://raw.githubusercontent.com/shrav2/Environmental-Plastic-Waste-Analysis/refs/heads/main/Nigeria%20Data.csv')
 
 }
